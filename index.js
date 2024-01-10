939d423761adc8b6d80059ca192fc0f44b3800c7
NikitaGhimire <nikitaghimire1@gmail.com>
2024-01-10 16:02:52 +1100

    Fixed the styling of the grids

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const mainGrid = document.getElementById('mainGrid');
-  for (let i = 0; i < 16*16; i++){
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    mainGrid.appendChild(gridItem);
-    gridItem.addEventListener('mouseover', ()=>{
-      gridItem.style.backgroundColor = 'blue';
-    });
+  let userInput = 16;
+  createGrid(userInput);
+  
+  const btn = document.querySelector('.js-create-grid');
+  
+  btn.addEventListener('click', ()=>{
+    userInput = prompt('Enter the number of squares per side');
+    if (userInput && userInput >= 1 && userInput <= 100){
+      createGrid(userInput);
+    }
+  });
+
+  function createGrid(size){
+      const mainGrid = document.getElementById('mainGrid');
+      mainGrid.innerHTML = '';
+      mainGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+      for (let i = 0; i < size*size; i++){
+      const gridItem = document.createElement('div');
+      gridItem.classList.add('grid-item');
+      mainGrid.appendChild(gridItem);
+      gridItem.addEventListener('mouseover', ()=>{
+        gridItem.style.backgroundColor = 'blue';
+      });
+      }
   }
   
-});
+});  
 
 
 
