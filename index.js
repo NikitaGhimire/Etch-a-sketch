e367028b93708855f583bf5f03cbb6ed459ef8af
NikitaGhimire <nikitaghimire1@gmail.com>
2024-01-10 11:20:07 +1100

    Created 16*16 grid of square divs using flexbox and javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const mainGrid = document.getElementById('mainGrid');
+  for (let i = 0; i < 16*16; i++){
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    mainGrid.appendChild(gridItem);
+  }
+});
