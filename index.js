a40236eae3333b3ff5181e0d05e34f973c65215f
NikitaGhimire <nikitaghimire1@gmail.com>
2024-01-10 11:34:57 +1100

    Added hover effect to the grid divs

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
     mainGrid.appendChild(gridItem);
+    gridItem.addEventListener('mouseover', ()=>{
+      gridItem.style.backgroundColor = 'blue';
+    });
   }
+  
 });
+
+
+
